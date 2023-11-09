fd00ed6a886e3718d4afec3e9f60085025e9a023
ritvikr <ritvik@vt.edu>
2023-11-08 20:03:25 -0500

    added about page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./Navbar.js"
 import Explore from './Components/Explore';
 import UserProfile from './Components/Profile';
 import MapPage from './Components/MapPage';
+
+import About from './Components/About.js';
 const App = () => {
   const [ageRange, setAgeRange] = useState([20, 80]);
   const [selectedTags, setSelectedTags] = useState([]);
@@ -13,7 +15,8 @@ const App = () => {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="about" element={<div>About Page</div>} />
+        <Route path="/" element={<About />} />
+        <Route path="about" element={<About />} />
         <Route path="explore" element={<Explore ageRange={ageRange} setAgeRange={setAgeRange} setSelectedTags={setSelectedTags} />} />
         <Route path="map" element={<MapPage></MapPage>} />
         <Route path="profile" element={<UserProfile ageRange={ageRange} selectedTags={selectedTags} />} /> 
